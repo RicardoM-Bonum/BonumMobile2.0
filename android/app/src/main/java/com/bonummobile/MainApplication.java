13675adf77f0ee51338765b2a17038dc76660f59
RicardoM-Bonum <ricardo@bonumcoaching.com>
2024-01-16 14:33:23 -0600

    Libraries tested in android, all good

--- android/app/src/main/java/com/bonummobile/MainApplication.java
+++ android/app/src/main/java/com/bonummobile/MainApplication.java
@@ -1,5 +1,6 @@
 package com.bonummobile;
 
+
 import android.app.Application;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
@@ -9,6 +10,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
+import com.lugg.RNCConfig.RNCConfigPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
