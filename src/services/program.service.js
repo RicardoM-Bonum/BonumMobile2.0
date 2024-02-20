850d6b0718969722c4fd2393dc4b167bdeb89c3b
Nan7s <136646903+Nan7s@users.noreply.github.com>
2024-02-20 15:09:27 -0300

    Update program.service.js

--- src/services/program.service.js
+++ src/services/program.service.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
-import {loadAbort} from 'utilities';
+import {loadAbort} from '../utilities';
 
 const route = `${process.env.REACT_APP_USERS_URL}/api/program`;
 
 export const getProgramById = id => {
-  console.log('route', route);
   const controller = loadAbort();
   return {
     call: () => axios.get(`${route}/${id}`, {signal: controller.signal}),
